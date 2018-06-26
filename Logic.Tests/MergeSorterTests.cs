a1fc56a7260999a5080fe2e4ba67b1ea146cf344
Domino <zhikhanastasya@gmail.com>
2018-06-26 23:10:51 +0300

    add (Logic.Tests) test for MergeSorter class and QuickSorter class

---
+++ Logic.Tests/MergeSorterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class MergeSorterTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Sort_NullArray_ReturnArgumentException()
+        {
+            int[] array = null;
+
+            var sorter = new MergeSorter<int>();
+
+            sorter.Sort(array);
+        }
+
+        [TestMethod]
+        public void Sort_IntArray_ReturnSortedArray()
+        {
+            int[] array = { 1, 6, 45, 6, 5, 35 };
+            int[] expected = { 1, 5, 6, 6, 35, 45 };
+
+            var sorter = new MergeSorter<int>();
+
+            sorter.Sort(array);
+
+            CollectionAssert.AreEqual(array, expected);
+        }
+    }
+}
---
+++ Logic.Tests/QuickSorterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class QuickSorterTests
+    {
+        [TestMethod]
+        public void Sort_IntArray_ReturnSortedArray()
+        {
+            int[] array = { 1, 6, 45, 6, 5, 35 };
+            int[] expected = { 1, 5, 6, 6, 35, 45 };
+
+            var sorter = new QuickSorter<int>();
+
+            sorter.Sort(array);
+
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestMethod]
+        public void Sort_StringArray_ReturnSortedArray()
+        {
+            string[] array = { "fgh", "a", "", "b", "fga", "" };
+            string[] expected = {"", "",  "a", "b", "fga", "fgh"};
+
+            var sorter = new QuickSorter<string>();
+
+            sorter.Sort(array);
+
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Sort_NullArray_ReturnArgumentException()
+        {
+            int[] array = null;
+
+            var sorter = new QuickSorter<int>();
+
+            sorter.Sort(array);
+        }
+    }
+}
